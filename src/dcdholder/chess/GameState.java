58562d9163baa59181af8f442efbab68ed0f9327
E0 <dcdholder@gmail.com>
2015-10-15 21:39:04 -0400

    Added methods to move all specific piece types with side effects + added methods and fields to display board in CLI + moved implementation of PieceSpecific to parent class Piece + implemented methods for modifying Piece list in GameState + added general-purpose methods to Coord and Move (actually do it this time)

--- src/dcdholder/chess/GameState.java
+++ src/dcdholder/chess/GameState.java
@@ -1,13 +1,61 @@
 package dcdholder.chess;
 import java.util.*;
+import java.math.*;
 
 //All conceivable moves for a given square are added to a list
 //This list is passed into an arbiter, which spits out the list of moves which are legal
 //This step is repeated for every PieceSpecific on the board
 //Game consists of a list of PieceSpecifics, perhaps mapped to Coords
 //PieceSpecifics themselves store some data (has this rook already moved? etc.)
+//TODO: write all of the arbiter methods for each piece
+//TODO: figure out a way to allow promotion to arbitrary piece type, not just queen
+//TODO: begin testing once CLI is done and arbiters have been implemented
 public class GameState {
 	Set<Piece> chessPieces;
+	//this is a list because pieces that have been captured may have identical state at the point of capture to pieces that were captured earlier
+	List<Piece> graveyard;
+	PieceColour currentPlayer;
+	
+	public String createBoardString() {
+		String boardString = new String();
+		String[] boardLine = new String[8];
+		String tmpLine = new String();
+		//first, draw the board from the perspective of the white player, then draw depending on the current player
+		//board is initially in the correct left-to-right order with respect to white
+		for(int j=1;j<=8;j++) {
+			for(int i=1;i<=8;i++) {
+				if(coordContainsPiece(new Coord(i,j))) {
+					boardLine[j-1] = boardLine + Character.toString(getPieceAtLocation(new Coord(i,j)).getRepChar());
+				} else {
+					if((i+j)%2==0) {
+						boardLine[j-1] = boardLine + Character.toString('■');
+					} else {
+						boardLine[j-1] = boardLine + Character.toString('□');
+					}
+				}
+			}
+		}
+		//orient correctly for the current player
+		if(currentPlayer==PieceColour.WHITE) {
+			for(int j=1;j<=8;j++) {
+				List<String> tmpList = Arrays.asList(boardLine);
+				Collections.reverse(tmpList);
+				boardLine = tmpList.toArray(boardLine);
+			}
+		} else if(currentPlayer==PieceColour.BLACK) {
+			for(int j=1;j<=8;j++) {
+				boardLine[j] = new StringBuilder(boardLine[j]).reverse().toString();
+			}
+		}
+		//transform into a single multiline string, so that this 
+		for(int j=1;j<=8;j++) {
+			boardString = boardString + boardLine[j] + "\n";
+		}
+		
+		return boardString;
+	}
+	
+	public void drawBoardCli() {System.out.println(createBoardString());}
 	
 	public Set<Move> getAllLegalMoves() {
 		Set<Move> allLegalMoves = new HashSet<Move>();
@@ -15,6 +63,62 @@ public class GameState {
 		//fill this in
 		
 		return allLegalMoves;
+	}
+	
+	public void removePieceAtLocation(Coord removeCoord) {
+		for(Piece chessPiece : chessPieces) {
+			if(chessPiece.getPieceCoord().equals(removeCoord)) {
+				chessPieces.remove(chessPiece);
+				graveyard.add(chessPiece);
+			}
+		}
+	}
+	
+	public void movePieceAtLocation(Coord initialCoord, Coord destCoord) {
+		boolean pieceFound = false;
+		
+		for(Piece chessPiece : chessPieces) {
+			if(chessPiece.getPieceCoord().equals(initialCoord)) {
+				chessPiece.makeMovePieceSpecific(new Move(initialCoord,destCoord));
+				pieceFound = true;
+				break;
+			}
+		}
+		
+		if(pieceFound==false) {
+			throw new IllegalArgumentException("Was not able to move piece at location " + initialCoord.toString() + " since none exists");
+		}
+	}
+	
+	public Piece getPieceAtLocation(Coord pieceCoord) {
+		boolean pieceFound = false;
+		Piece desiredPiece = new Pawn();
+		
+		for(Piece chessPiece : chessPieces) {
+			if(chessPiece.getPieceCoord().equals(pieceCoord)) {
+				desiredPiece = chessPiece;
+				pieceFound = true;
+				break;
+			}
+		}
+		
+		if(pieceFound==false) {
+			throw new IllegalArgumentException("Was not able to get piece at location " + pieceCoord.toString() + " since none exists");
+		}
+		return desiredPiece;
+	}
+	
+	public boolean coordContainsPiece(Coord pieceCoord) {
+		boolean pieceExistsAtCoord = false;
+		
+		for(Piece chessPiece : chessPieces) {
+			if(chessPiece.getPieceCoord().equals(pieceCoord)) {
+				pieceExistsAtCoord = true;
+				break;
+			}
+		}
+		
+		return pieceExistsAtCoord;
 	}
 	
 	enum PieceColour {
@@ -33,12 +137,22 @@ public class GameState {
 		Coord(int x, byte y) {this((byte)x,y);}
 		
 		boolean bothDimsBetween1And8() {if((this.x<=8&&this.x>=1)&&(this.y<=8&&this.y>=1)) {return true;} else {return false;}}
-		Coord addCoords(Coord coordA, Coord coordB) {
+		
+		public Coord addCoords(Coord coordA, Coord coordB) {
 			int newCoordX = coordA.x + coordB.x;
 			int newCoordY = coordA.y + coordB.y;
 			
 			return new Coord(newCoordX,newCoordY);
 		}
+		
+		public Coord subCoords(Coord coordA, Coord coordB) {
+			int newCoordX = coordB.x - coordA.x;
+			int newCoordY = coordB.y - coordA.y;
+			
+			return new Coord(newCoordX,newCoordY);
+		}
+		
+		public Coord absCoord() {return new Coord(Math.abs(this.x),Math.abs(this.y));}
 		
 		public String toString() {return "X value: " + this.x + " ,Y value: " + this.y;}
 		
@@ -80,6 +194,39 @@ public class GameState {
 			return wouldBeLegalMoveObject(init,dummyCoord.addCoords(init,relative));
 		}
 		
+		public Coord coordDeltaFromMove(Move move) {
+			Coord dummyCoord = new Coord();
+			
+			return dummyCoord.subCoords(move.dest,move.init);
+		}
+		
+		public Coord absCoordDeltaFromMove(Move move) {
+			Coord returnCoord = coordDeltaFromMove(move);
+			
+			return returnCoord.absCoord();
+		}
+		
+		public boolean moveIsToDirection(Move move, String direction) {
+			boolean isToDirection = false;
+			
+			switch (direction) {
+				case "right":
+					if(move.dest.getX()>move.init.getX()) {isToDirection=true;}
+					break;
+				case "left":
+					if(move.dest.getX()<move.init.getX()) {isToDirection=true;}
+					break;
+				case "up":
+					if(move.dest.getY()>move.init.getY()) {isToDirection=true;}
+					break;
+				case "down":
+					if(move.dest.getY()<move.init.getY()) {isToDirection=true;}
+					break;
+			}
+			
+			return isToDirection;
+		}
+		
 		public String toString() {
 			return "Initial coord: {" + this.init.toString() + "}, Destination coord: {" + this.dest.toString() + "}";
 		}
@@ -114,10 +261,15 @@ public class GameState {
 		}
 	}
 	
-	abstract public class Piece {
+	abstract public class Piece implements PieceSpecific {
+		//REP_CHAR is set by pieces individually according to type and colour
+		char REP_CHAR = 'X';
 		Coord pieceCoord;
 		PieceColour pieceColour;
 		Set<Coord> relativeCoordSet;
+		
+		public Coord getPieceCoord() {return this.pieceCoord;}
+		public char getRepChar() {return this.REP_CHAR;}
 		
 		public Set<Move> formMovesFromRelativeCoordSet() {
 			Set<Move> testableMoveSet = new HashSet<Move>();
@@ -131,6 +283,11 @@ public class GameState {
 			}
 			
 			return testableMoveSet;
+		}
+		
+		public void makeMovePieceCommon(Move move) {
+			removePieceAtLocation(move.dest);
+			pieceCoord = new Coord(move.getDest());
 		}
 		
 		public boolean isMoveLegalCommon(Move moveAttempt) {
@@ -151,19 +308,46 @@ public class GameState {
 	}
 	
 	private interface PieceSpecific {
+		//public attemptMovePieceSpecific(Move attemptMove);
 		//TODO: implement move legality checking in a meaningful way for each piece type
+		public void makeMovePieceSpecific(Move move);
+		//TODO: rename instances of moveAttempt to attemptMove
 		public boolean isMoveLegalPieceSpecific(Move moveAttempt);
 		public Set<Coord> testableRelativeCoord();
 	}
 	
-	public class Pawn extends Piece implements PieceSpecific {
-		boolean lastMoveWasDouble;
+	public class Pawn extends Piece {
+		private boolean lastMoveWasDouble;
 		
 		public boolean isMoveLegalPieceSpecific(Move moveAttempt) {
 			if(!isMoveLegalCommon(moveAttempt)) {
 				return false;
 			} else {
 				return false;
+			}
+		}
+		//makeMove should only be called through attemptMove, which should validate it
+		public void makeMovePieceSpecific(Move move) {
+			Move dummyMove = new Move();
+			
+			//check if move is an en passant... if so, remove other piece
+			if(dummyMove.absCoordDeltaFromMove(move).equals(new Coord(1,1)) && !coordContainsPiece(move.dest)) {
+				if(dummyMove.moveIsToDirection(move,"right")) {
+					removePieceAtLocation(new Coord(move.getInit().getX()+1,move.getInit().getY()));
+				} else if (dummyMove.moveIsToDirection(move,"left")) {
+					removePieceAtLocation(new Coord(move.getInit().getX()-1,move.getInit().getY()));
+				}
+			}
+			makeMovePieceCommon(move);
+			//check if the last move was a double move
+			if(dummyMove.absCoordDeltaFromMove(move).equals(new Coord(0,2))) {this.lastMoveWasDouble=true;}
+			//take double moves off the table if the piece has already been moved
+			relativeCoordSet.remove(new Coord(0,2));
+			//check if the move results in pawn promotion... if so, create new piece and remove self
+			if((pieceColour==PieceColour.WHITE && move.dest.getY()==8) || (pieceColour==PieceColour.BLACK && move.dest.getY()==1)) {
+				//produce a new queen at current location and delete self
+				chessPieces.add(new Queen(pieceColour,move.getDest()));
+				chessPieces.remove(this);
 			}
 		}
 		public Set<Coord> testableRelativeCoord() {
@@ -179,15 +363,32 @@ public class GameState {
 			return testableRelativeCoord;
 		}
 		
+		//TODO: only used when dummy pieces are necessary: find a way to obviate
+		Pawn() {
+			super(PieceColour.WHITE,new Coord(1,1));
+			this.lastMoveWasDouble = false;
+			//potential for performance penalty
+			this.relativeCoordSet = testableRelativeCoord();
+		}
+		
 		Pawn(PieceColour pieceColour,Coord pieceCoord) {
 			super(pieceColour,pieceCoord);
+			if(pieceColour==PieceColour.WHITE) {
+				super.REP_CHAR = '♙';
+			} else if(pieceColour==PieceColour.BLACK) {
+				super.REP_CHAR = '♟';
+			}
 			this.lastMoveWasDouble = false;
 			this.relativeCoordSet = testableRelativeCoord();
 		}
 	}
-	public class Rook extends Piece implements PieceSpecific {
+	public class Rook extends Piece {
 		boolean hasMoved;
 		
+		public void makeMovePieceSpecific(Move move) {
+			makeMovePieceCommon(move);
+			this.hasMoved = true;
+		}
 		public boolean isMoveLegalPieceSpecific(Move moveAttempt) {
 			if(!isMoveLegalCommon(moveAttempt)) {
 				return false;
@@ -210,12 +411,20 @@ public class GameState {
 		
 		Rook(PieceColour pieceColour,Coord pieceCoord) {
 			super(pieceColour,pieceCoord);
+			if(pieceColour==PieceColour.WHITE) {
+				super.REP_CHAR = '♖';
+			} else if(pieceColour==PieceColour.BLACK) {
+				super.REP_CHAR = '♜';
+			}
 			this.hasMoved = false;
 			this.relativeCoordSet = testableRelativeCoord();
 		}
 	}
-	public class Bishop extends Piece implements PieceSpecific {
+	public class Bishop extends Piece {
 		
+		public void makeMovePieceSpecific(Move move) {
+			makeMovePieceCommon(move);
+		}
 		public boolean isMoveLegalPieceSpecific(Move moveAttempt) {
 			if(!isMoveLegalCommon(moveAttempt)) {
 				return false;
@@ -238,11 +447,19 @@ public class GameState {
 		
 		Bishop(PieceColour pieceColour,Coord pieceCoord) {
 			super(pieceColour,pieceCoord);
+			if(pieceColour==PieceColour.WHITE) {
+				super.REP_CHAR = '♗';
+			} else if(pieceColour==PieceColour.BLACK) {
+				super.REP_CHAR = '♝';
+			}
 			this.relativeCoordSet = testableRelativeCoord();
 		}
 	}
-	public class Knight extends Piece implements PieceSpecific {
+	public class Knight extends Piece {
 		
+		public void makeMovePieceSpecific(Move move) {
+			makeMovePieceCommon(move);
+		}
 		public boolean isMoveLegalPieceSpecific(Move moveAttempt) {
 			if(!isMoveLegalCommon(moveAttempt)) {
 				return false;
@@ -269,11 +486,19 @@ public class GameState {
 		
 		Knight(PieceColour pieceColour,Coord pieceCoord) {
 			super(pieceColour,pieceCoord);
+			if(pieceColour==PieceColour.WHITE) {
+				super.REP_CHAR = '♘';
+			} else if(pieceColour==PieceColour.BLACK) {
+				super.REP_CHAR = '♞';
+			}
 			this.relativeCoordSet = testableRelativeCoord();
 		}
 	}
-	public class Queen extends Piece implements PieceSpecific {
+	public class Queen extends Piece {
 		
+		public void makeMovePieceSpecific(Move move) {
+			makeMovePieceCommon(move);
+		}
 		public boolean isMoveLegalPieceSpecific(Move moveAttempt) {
 			if(!isMoveLegalCommon(moveAttempt)) {
 				return false;
@@ -304,12 +529,43 @@ public class GameState {
 		
 		Queen(PieceColour pieceColour,Coord pieceCoord) {
 			super(pieceColour,pieceCoord);
+			if(pieceColour==PieceColour.WHITE) {
+				super.REP_CHAR = '♕';
+			} else if(pieceColour==PieceColour.BLACK) {
+				super.REP_CHAR = '♛';
+			}
 			this.relativeCoordSet = testableRelativeCoord();
 		}
 	}
-	public class King extends Piece implements PieceSpecific {
+	public class King extends Piece {
 		boolean hasMoved;
 		
+		public void makeMovePieceSpecific(Move move) {
+			Move dummyMove = new Move();
+			
+			makeMovePieceCommon(move);
+			this.hasMoved = true;
+			//remove castle from list of possible moves
+			relativeCoordSet.remove(new Coord(2,0));
+			relativeCoordSet.remove(new Coord(-2,0));
+			//check if you are castling
+			if(dummyMove.absCoordDeltaFromMove(move)==new Coord(2,0)) {
+				//castle with the rook located at the rook's origin which is in the direction of the castle
+				if(dummyMove.moveIsToDirection(move,"right")) {
+					if(pieceColour==PieceColour.WHITE) {
+						movePieceAtLocation(new Coord(8,1),new Coord(6,1));
+					} else if(pieceColour==PieceColour.BLACK) {
+						movePieceAtLocation(new Coord(8,8),new Coord(6,8));
+					}
+				} else if (dummyMove.moveIsToDirection(move,"left")) {
+					if(pieceColour==PieceColour.WHITE) {
+						movePieceAtLocation(new Coord(1,1),new Coord(4,1));
+					} else if(pieceColour==PieceColour.BLACK) {
+						movePieceAtLocation(new Coord(1,8),new Coord(4,8));
+					}
+				}
+			}
+		}
 		public boolean isMoveLegalPieceSpecific(Move moveAttempt) {
 			if(!isMoveLegalCommon(moveAttempt)) {
 				return false;
@@ -334,6 +590,11 @@ public class GameState {
 		
 		King(PieceColour pieceColour,Coord pieceCoord) {
 			super(pieceColour,pieceCoord);
+			if(pieceColour==PieceColour.WHITE) {
+				super.REP_CHAR = '♔';
+			} else if(pieceColour==PieceColour.BLACK) {
+				super.REP_CHAR = '♚';
+			}
 			this.hasMoved = false;
 			this.relativeCoordSet = testableRelativeCoord();
 		}
@@ -385,6 +646,7 @@ public class GameState {
 	
 	GameState() {
 		this.chessPieces = new HashSet<Piece>();
+		this.graveyard = new ArrayList<Piece>();
 		
 		//add pawns for both sides
 		for(int i=1;i<=8;i++) {
